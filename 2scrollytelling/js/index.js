fa87d3b5afc29e22a5c6e1459b4b05227518454f
sirciv <spencerallan8@gmail.com>
2021-12-12 14:30:05 -0600

    scrollytelling project update

--- 2scrollytelling/js/index.js
+++ 2scrollytelling/js/index.js
@@ -1,15 +1,17 @@
 const pageAnimations = gsap.timeline();
 
 pageAnimations.from(".two", {yPercent: 100})
+.from(".seven", {yPercent: 100})
 .from(".three", {yPercent: 100})
 .from(".four", {yPercent: 100})
 .from(".five", {yPercent: 100})
+.from(".six", {yPercent: 100})
 
 ScrollTrigger.create({
 	animation: pageAnimations,
 	trigger: "article",
 	start: "top top",
-   end: "+=500%",
+   end: "+=700%",
 	scrub: true,
    pin:true,
 });
